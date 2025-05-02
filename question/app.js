cbd90a2d39dd0e50515af344951c305298599c53
Yuya Saito <wasd132121@gmail.com>
2025-05-02 16:10:00 +0900

    first commit

---
+++ question/app.js
@@ -0,0 +1,12 @@
+for (let i = 0; i < 10; i++) {
+  console.log(i + '回目のコンソール');
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  sum += numbers[i];
+}
+
+console.log(sum);
